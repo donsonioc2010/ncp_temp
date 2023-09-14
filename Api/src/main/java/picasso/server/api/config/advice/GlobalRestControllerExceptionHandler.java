e62e37a759cfdf61c357d42017d4466783e7e552
donsonioc2010 <whddnjs822@gmail.com>
2023-09-15 00:22:11 +0900

    feat : ExceptionHandlerAdvice 구현 - RestControllerAdvice 구현 - ControllerAdvice 구현
    문제점 : 두개가 일단 동일한 작업을 하는 코드여서 추후 기능으로 따로 뺴는 작업 필요

---
+++ Api/src/main/java/picasso/server/api/config/advice/GlobalRestControllerExceptionHandler.java
@@ -0,0 +1,36 @@
+package picasso.server.api.config.advice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import picasso.server.common.dto.ErrorDetail;
+import picasso.server.common.exception.BaseException;
+import picasso.server.common.exception.GlobalException;
+
+//TODO : 현재 `GlobalControllerExceptionHandler`와 동일한 코드, 추후 수정 필요
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class GlobalRestControllerExceptionHandler {
+    @ExceptionHandler(BaseException.class)
+    protected ResponseEntity<ErrorDetail> baseExceptionHandle(BaseException e, HttpServletRequest request) {
+        ErrorDetail errorDetail = e.getErrorCode().getErrorDetail();
+        log.warn("ExceptionName >>> {}, ErrorCode >>> {}, ExceptionReason >>> {}",
+                e.getClass(), errorDetail.getStatusCode(), errorDetail.getReason());
+
+        return ResponseEntity.status(errorDetail.getStatusCode()).body(errorDetail);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorDetail> argumentNotValidHandle(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ErrorDetail errorDetail = ErrorDetail.builder()
+                .statusCode(GlobalException.METHOD_ARGUMENT_ERROR.getStatusCode())
+                .reason(GlobalException.METHOD_ARGUMENT_ERROR.getReason())
+                .build();
+        return ResponseEntity.status(errorDetail.getStatusCode()).body(errorDetail);
+    }
+}
