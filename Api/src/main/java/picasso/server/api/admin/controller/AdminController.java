9c4419621aea594b343746bde8f1a2d3f3a9c147
Romangduck <rrlagusejr@naver.com>
2023-09-15 20:30:20 +0900

    admin_list , detail in Controller -ing

--- Api/src/main/java/picasso/server/api/admin/controller/AdminController.java
+++ Api/src/main/java/picasso/server/api/admin/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import picasso.server.api.admin.dto.response.BoardResponseDTO;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +22,29 @@ public class AdminController {
                 .id(1L)
                 .startAmount(10000L)
                 .title("제목1")
+                        .painter("고흐")
+                        .created_at(Timestamp.valueOf("2023-09-15"))
+
+
                 .build());
 
 
         return "admin/list";
+    }
+
+    @GetMapping("/detail")
+    public String adminBoarddetail() {
+        List<BoardResponseDTO> detail = new ArrayList<>();
+        detail.add(BoardResponseDTO.builder()
+                .photo_link("www.url")
+                .id(1L)
+                .startAmount(10000L)
+                .title("제목1")
+                .painter("고흐")
+                .created_at(Timestamp.valueOf("2023-09-15"))
+                .content("이 그림은 1928년 영국에서")
+                .status("대기중")
+                .build());
+    return "admin/detail";
     }
 }
--- Api/src/main/java/picasso/server/api/admin/dto/response/BoardResponseDTO.java
+++ Api/src/main/java/picasso/server/api/admin/dto/response/BoardResponseDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -11,4 +13,9 @@ public class BoardResponseDTO {
     private long id;
     private long startAmount;
     private String title;
+    private Timestamp created_at;
+    private String status;
+    private String painter;
+    private String content;
+    private String photo_link;
 }
