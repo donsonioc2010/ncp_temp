b8b38f794386d92b8de2aad102e14480a09b3cc9
bongsh0112 <bongsh0112@gmail.com>
2023-09-18 16:52:28 +0900

    feat: 결제 연동 구현 thymeleaf 오류 수정 1트 #29

--- Api/src/main/resources/static/js/payment.js
+++ Api/src/main/resources/static/js/payment.js
@@ -1,58 +1,51 @@
-console.log("html & js connection complete!");
-
 var IMP = window.IMP;
-IMP.init("imp16618334");
+IMP.init("imp16618334"); // 재발급 받은 뒤 숨겨버릴 예정
 
-function requestKakaoPay() {
-    IMP.request_pay({
-        pg : "kakaopay.TC0ONETIME",
+var requestPayment = pg_name  => {
+    return IMP.request_pay({
+        pg : pg_name,
         pay_method : 'card',
-        merchant_uid: "09090902", // 계속 바뀌게 설정해야함. 결제에서 가장 중요한 정보 -> 이걸로 결제 하나하나를 식별함
-        name : '당근 10kg',
+        merchant_uid: "picasso_" + new Date().getMilliseconds(), // 계속 바뀌게 설정해야함. 결제에서 가장 중요한 정보 -> 이걸로 결제 하나하나를 식별함
+        name : 'asdfasdf',
         amount : 1004,
         buyer_email : 'Iamport@chai.finance',
         buyer_name : '아임포트 기술지원팀'
-    }, function (rsp) { // callback
-        if (rsp.success) {
-            console.log(rsp);
+    });
+}
+
+var paymentResult = pg_name => {
+    return (requestPayment(pg_name), function(response) {
+        if (response.success) {
+            var msg = "결제 완료";
+            msg += '고유ID : ' + response.imp_uid;
+            msg += '// 상점 거래ID : ' + response.merchant_uid;
+            msg += '// 결제 금액 : ' + response.paid_amount;
+            msg += '// 카드 승인번호 : ' + response.apply_num;
+
+            postPayInfo(true);
+            console.log(msg);
         } else {
-            console.log(rsp);
+            var msg = "결제 실패"
+            msg += "에러 내용" + response.error_msg;
+            postPayInfo(false);
+            console.log(msg);
         }
     });
 }
 
-function requestTossPay() {
-    IMP.request_pay({
-        pg : "tosspay.tosstest",
-        pay_method : 'card',
-        merchant_uid: "1341",
-        name : '당근 10kg',
-        amount : 1004,
-        buyer_email : 'Iamport@chai.finance',
-        buyer_name : '아임포트 기술지원팀'
-    }, function (rsp) { // callback -> rsp에
-        if (rsp.success) {
-            console.log(rsp);
-        } else {
-            console.log(rsp);
-        }
-    });
-}
-
-function requestKGInicis() {
-    IMP.request_pay({
-        pg : "html5_inicis.INIBillTst",
-        pay_method : 'card',
-        merchant_uid: "57854673",
-        name : '당근 10kg',
-        amount : 1004,
-        buyer_email : 'Iamport@chai.finance',
-        buyer_name : '아임포트 기술지원팀'
-    }, function (rsp) { // callback
-        if (rsp.success) {
-            console.log(rsp);
-        } else {
-            console.log(rsp);
-        }
-    });
+function postPayInfo(tof, rsp) {
+    $.ajax({
+        type : 'post',
+        url : '/pay',
+        data : {
+            "pay_result" : tof,
+            "buyer_name" : rsp.buyer_name,
+            "buyer_email" : rsp.buyer_email,
+            "merchant_uid" : rsp.merchant_uid,
+            "product_name" : rsp.name,
+            "pg_provider" : rsp.pg_provider,
+            "amount" : rsp.paid_amount,
+            "pay_method" : rsp.pay_method
+        },
+    })
 }
