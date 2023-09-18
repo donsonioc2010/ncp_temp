e7060e83b8eb19579dbb465769d800e6e6f9eb5c
bongsh0112 <bongsh0112@gmail.com>
2023-09-18 10:17:07 +0900

    feat: 결제 연동 구현에 맞춘 dbml 수정 #29

--- Api/src/main/resources/static/js/payment.js
+++ Api/src/main/resources/static/js/payment.js
@@ -7,14 +7,11 @@ function requestKakaoPay() {
     IMP.request_pay({
         pg : "kakaopay.TC0ONETIME",
         pay_method : 'card',
-        merchant_uid: "52300234", // 계속 바뀌게 설정해야함
+        merchant_uid: "09090902", // 계속 바뀌게 설정해야함. 결제에서 가장 중요한 정보 -> 이걸로 결제 하나하나를 식별함
         name : '당근 10kg',
         amount : 1004,
         buyer_email : 'Iamport@chai.finance',
-        buyer_name : '아임포트 기술지원팀',
-        buyer_tel : '010-1234-5678',
-        buyer_addr : '서울특별시 강남구 삼성동',
-        buyer_postcode : '123-456'
+        buyer_name : '아임포트 기술지원팀'
     }, function (rsp) { // callback
         if (rsp.success) {
             console.log(rsp);
@@ -28,15 +25,12 @@ function requestTossPay() {
     IMP.request_pay({
         pg : "tosspay.tosstest",
         pay_method : 'card',
-        merchant_uid: "52300234", // 계속 바뀌게 설정해야함
+        merchant_uid: "1341",
         name : '당근 10kg',
         amount : 1004,
         buyer_email : 'Iamport@chai.finance',
-        buyer_name : '아임포트 기술지원팀',
-        buyer_tel : '010-1234-5678',
-        buyer_addr : '서울특별시 강남구 삼성동',
-        buyer_postcode : '123-456'
-    }, function (rsp) { // callback
+        buyer_name : '아임포트 기술지원팀'
+    }, function (rsp) { // callback -> rsp에
         if (rsp.success) {
             console.log(rsp);
         } else {
@@ -49,14 +43,11 @@ function requestKGInicis() {
     IMP.request_pay({
         pg : "html5_inicis.INIBillTst",
         pay_method : 'card',
-        merchant_uid: "52300234", // 계속 바뀌게 설정해야함
+        merchant_uid: "57854673",
         name : '당근 10kg',
         amount : 1004,
         buyer_email : 'Iamport@chai.finance',
-        buyer_name : '아임포트 기술지원팀',
-        buyer_tel : '010-1234-5678',
-        buyer_addr : '서울특별시 강남구 삼성동',
-        buyer_postcode : '123-456'
+        buyer_name : '아임포트 기술지원팀'
     }, function (rsp) { // callback
         if (rsp.success) {
             console.log(rsp);
