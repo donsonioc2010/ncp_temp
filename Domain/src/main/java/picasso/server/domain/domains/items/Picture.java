946afbb53b41ca2e0d3ab7b3a5caae34560e2a5a
dhgus44 <dhgus44@naver.com>
2023-09-18 18:59:07 +0900

    feat : 그림 경매품 게시물 Entity 추가 - Entity, Repository 추가

---
+++ Domain/src/main/java/picasso/server/domain/domains/items/Picture.java
@@ -0,0 +1,33 @@
+package picasso.server.domain.domains.items;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class Picture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long picture_id;
+
+    private String imgUrl; //그림 url
+
+    private String pictureName; //그림 이름
+
+    private String painterName; //화가 이름
+
+    @Column(nullable = false)
+    private String details; //그림 설명
+
+    private int startingPrice; //시작 가격
+
+    private int incrementAmount; //최소 입찰 단위
+
+    private String size; //그림 사이즈
+    private LocalDateTime dateTime;
+
+}
