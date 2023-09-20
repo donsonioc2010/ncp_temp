49ec00982884a40cf7f98a09ce1e88263daebcba
bongsh0112 <bongsh0112@gmail.com>
2023-09-20 14:27:33 +0900

    edit: 결제 DBML 수정 + 결제 API 초안 갈아엎기 #29

--- Api/src/main/java/picasso/server/api/exchange/model/dto/ExchangeDTO.java
+++ Api/src/main/java/picasso/server/api/exchange/model/dto/ExchangeDTO.java
@@ -9,25 +9,6 @@ import picasso.server.domain.domains.items.PayMethod;
 @Getter
 @Builder
 public class ExchangeDTO {
-  
-  private PGName pgName;
-  private PayMethod payMethod;
-  private String merchantUUID;
-  private String productName;
-  private int price;
-  
-//  TODO: User Entity 작성 시 추가 작성 예정
-//  private String userName;
-//  private String userEmail;
-  
-  public static ExchangeDTO of(PaymentHistory paymentHistory) {
-    return ExchangeDTO.builder()
-            .pgName(paymentHistory.getPgName())
-            .payMethod(paymentHistory.getPayMethod())
-            .merchantUUID(paymentHistory.getMerchantUUID())
-            .productName(paymentHistory.getProductName())
-            .price(paymentHistory.getPrice())
-            .build();
-  }
+
   
 }
--- Api/src/main/java/picasso/server/api/exchange/model/request/PostCreateExchangeRequest.java
+++ Api/src/main/java/picasso/server/api/exchange/model/request/PostCreateExchangeRequest.java
@@ -9,8 +9,4 @@ import picasso.server.api.exchange.model.dto.ExchangeDTO;
 @NoArgsConstructor
 public class PostCreateExchangeRequest {
 
-  // TODO: rsp에 들어오는 데이터들에서 필요한 것을 더 추가할 예정.
-  
-  @Valid
-  private ExchangeDTO exchangeDTO;
 }
