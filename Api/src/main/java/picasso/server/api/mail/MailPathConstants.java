637f1b71f0c16b1a7c09070473c904e4a88cfec7
donsonioc2010 <whddnjs822@gmail.com>
2023-09-25 20:15:25 +0900

    feat : 이메일 구현 - 입찰자가 없는 경우 유찰 안내메일 - 관리자가 승인하지 않은 경우 유찰 안내메일 - 경매 시작 안내 메일 - 링크 발송을 위한 Properties 추가

--- Api/src/main/java/picasso/server/api/mail/MailPathConstants.java
+++ Api/src/main/java/picasso/server/api/mail/MailPathConstants.java
@@ -7,16 +7,17 @@ public class MailPathConstants {
     public static final String PICTURE_NOT_APPROVE_REJECT_MAIL = "mail/not-approve-reject-mail";
 
     // 입찰자가 없음으로 인한 유찰메일 안내
-    public static final String PICUTRE_NO_BID_REJECT_MAIL = "mail/no-bid-reject-mail";
-
-    // 게시물 관리자 승인 안내 메일
-    public static final String PICTURE_APPROVE_MAIL = "mail/approve-picture-mail";
+    public static final String PICTURE_NO_BID_REJECT_MAIL = "mail/no-bid-reject-mail";
 
     // 경매 낙찰 안내 메일
     public static final String PICTURE_SUCCESSBID_MAIL = "mail/success-bid-picture-mail";
 
     // 경매 시작 안내 메일
     public static final String PICTURE_BIDDING_MAIL = "mail/bidding-picture-mail";
+
+    /*TODO : 시간 될 떄 추가구현!*/
+    // 게시물 관리자 승인 안내 메일
+    public static final String PICTURE_APPROVE_MAIL = "mail/approve-picture-mail";
 
     // 입찰자 추가 발생 안내 ( 판매자 )
     public static final String PICTURE_NEW_BID_SELLER_MAIL = "mail/new-bid-picture-mail-seller";
