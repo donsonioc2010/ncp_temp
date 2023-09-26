169ab7ec91cb8fcd14dae9199dc28e83750b88b8
raeraeraee <rlatjsfo674@naver.com>
2023-09-26 11:05:10 +0900

    feat : MyPage 개발 - 마이페이구현

--- Domain/src/main/java/picasso/server/domain/domains/user/repository/MypageRepository.java
+++ Domain/src/main/java/picasso/server/domain/domains/user/repository/MypageRepository.java
@@ -1,7 +1,13 @@
 package picasso.server.domain.domains.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import picasso.server.domain.domains.dto.UserDTO;
 import picasso.server.domain.domains.user.entity.User;
 
 public interface MypageRepository extends JpaRepository <User, Long> {
+  @Query("SELECT new picasso.server.domain.domains.dto.UserDTO(u.nickName, u.point, u.profile) FROM User u WHERE u.id = :userId")
+  UserDTO findUserFieldsById(@Param("userId") Long userId);
+
 }
